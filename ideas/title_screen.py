d9a1b94cae4aa140155597dbca24cb941518fa54
Markus Appel <markus.appel@adyton.net>
2015-03-28 16:37:10 +0100

    curses panel experiments in title screen idea file

--- ideas/title_screen.py
+++ ideas/title_screen.py
@@ -1,41 +1,45 @@
 #!/usr/bin/env python3.4
 
 import curses
+import curses.panel
 
 def run(stdscr):
     logo = [line for line in open("front_logo.txt")]
     logo_height = len(logo)
     logo_width = len(logo[0])
 
-    logo_win = curses.newwin(
+    main_pane = curses.panel.new_panel(curses.newwin(
+        curses.LINES, curses.COLS, 0, 0
+    ))
+    logo_pane = curses.panel.new_panel(curses.newwin(
         logo_height+5, logo_width+2, 4, curses.COLS//2 - logo_width//2
-    )
-    upper_win = curses.newwin(
+    ))
+    name_pane = curses.panel.new_panel(curses.newwin(
         3, 32, int(curses.LINES * 0.7), curses.COLS//2 - 16
-    )
-    lower_win = curses.newwin(
+    ))
+    ip_pane = curses.panel.new_panel(curses.newwin(
         3, 32, int(curses.LINES * 0.7) + 6, curses.COLS//2 - 16
-    )
+    ))
 
-    logo_win.box()
-    upper_win.box()
-    lower_win.box()
+    logo_pane.window().box()
+    name_pane.window().box()
+    ip_pane.window().box()
 
     curses.init_pair(1, curses.COLOR_CYAN, curses.COLOR_BLUE)
     curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_WHITE)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_GREEN)
     curses.init_pair(4, curses.COLOR_BLACK, curses.COLOR_BLUE)
-    stdscr.bkgd('~', curses.color_pair(1))
-    logo_win.bkgd(' ', curses.color_pair(3))
-    upper_win.bkgd(' ', curses.color_pair(2))
-    lower_win.bkgd(' ', curses.color_pair(2))
+    main_pane.window().bkgd('~', curses.color_pair(1))
+    logo_pane.window().bkgd(' ', curses.color_pair(3))
+    name_pane.window().bkgd(' ', curses.color_pair(2))
+    ip_pane.window().bkgd(' ', curses.color_pair(2))
 
-    stdscr.addstr(curses.LINES//2, 8, '▲', curses.color_pair(4))
-    stdscr.addstr(curses.LINES//2 + 1, 8, '▣', curses.color_pair(4))
-    stdscr.addstr(curses.LINES//2 + 2, 8, '▣', curses.color_pair(4))
-    stdscr.addstr(curses.LINES//2 + 3, 8, '▼', curses.color_pair(4))
+    main_pane.window().addstr(curses.LINES//2, 8, '▲', curses.color_pair(4))
+    main_pane.window().addstr(curses.LINES//2 + 1, 8, '▣', curses.color_pair(4))
+    main_pane.window().addstr(curses.LINES//2 + 2, 8, '▣', curses.color_pair(4))
+    main_pane.window().addstr(curses.LINES//2 + 3, 8, '▼', curses.color_pair(4))
 
-    stdscr.addstr(
+    main_pane.window().addstr(
         curses.LINES//2, curses.COLS-15, '◀-▣-▣-▶',
         curses.color_pair(4)
     )
@@ -43,24 +47,32 @@ def run(stdscr):
     for y, row in enumerate(logo):
         for x, cell in enumerate(row.rstrip()):
             try:
-                logo_win.addstr(y+1, x+1, cell)
+                logo_pane.window().addstr(y+1, x+1, cell)
             except curses.error:
                 pass
-    upper_win.addstr(1, 3, "Your name: ", curses.A_BOLD)
-    lower_win.addstr(1, 3, "Host IP:   ", curses.A_BOLD)
+    name_pane.window().addstr(1, 3, "Your name: ", curses.A_BOLD)
+    ip_pane.window().addstr(1, 3, "Host IP:   ", curses.A_BOLD)
 
-    stdscr.refresh()
-    logo_win.refresh()
-    upper_win.refresh()
-    lower_win.refresh()
+    main_pane.window().refresh()
+    logo_pane.window().refresh()
+    name_pane.window().refresh()
+    ip_pane.window().refresh()
 
     curses.echo()
-    upper_win.getstr(1, 14, 15)
-    lower_win.getstr(1, 14, 15)
+    name_pane.window().getstr(1, 14, 15)
+    ip_pane.window().getstr(1, 14, 15)
     curses.noecho()
 
+    ip_pane.hide()
+    name_pane.hide()
+    logo_pane.move(
+        5, 5
+        #curses.LINES//2 - logo_height//2, curses.COLS//2 - logo_width//2
+    )
+    logo_pane.window().refresh()
+
     curses.curs_set(False)
-    upper_win.getch()
+    main_pane.window().getch()
 
 
 curses.wrapper(run)
