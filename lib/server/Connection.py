5c9005de13a1795186d53540a5c3480281b5c0ea
Sun0fABeach <markospoparkos@gmail.com>
2015-04-29 13:25:42 +0200

    Implement player info exchange on server

--- lib/server/Connection.py
+++ lib/server/Connection.py
@@ -27,6 +27,47 @@ class Connection(BaseConnection):
             sender.send(ShutdownMessage())
 
 
+    def setup_identification(self):
+        player_names = []
+        for player_id in range(2):
+            other_id = self._other_player_id(player_id)
+            name_msg = self._get_message()
+            player_name = name_msg.player_name
+            player_names.append(player_name)
+            self._msg_senders[other_id].send(IDMessage(other_id, player_name))
+
+        return player_names
+
+
+    def exchange_placements(self):
+        pass
+
+
+    def receive_shot(self):
+        pass
+
+
+    def inform_shot_result(self):
+        pass
+
+
+    def _abortion_check(self, message):
+        """
+        checks whether the given message signals the exit of a remote player.
+        if that is the case, it informs the other remote player about the exit
+        and raises the appropriate exception.
+        :param message: the message to be checked
+        """
+        if isinstance(message, ExitMessage):
+            other_id = _other_player_id(ExitMessage.player_id)
+            self._msg_senders[other_id].send(ExitMessage())
+            raise OpponentLeft
+
+
+    def _other_player_id(self, player_id):
+        return abs(player_id - 1)
+
+
     class MessageListener(BaseConnection.MessageListener):
 	    """
 	    daemon thread that puts all incoming messages into a message queue.
